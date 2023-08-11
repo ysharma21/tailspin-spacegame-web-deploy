c7d12518fea4f1e06aaf1a9d22818ca4717917e5
ysharma21 <yachnasharma@microsoft.com>
2023-08-10 21:38:02 -0400

    updating pytest

--- evaluation/test_evaluation.py
+++ evaluation/test_evaluation.py
@@ -1,12 +1,18 @@
-import unittest
+import os
+import pytest
+import xmlrunner
 import evaluation
 
-class TestEvaluation(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(evaluation.add(2, 3), 5)
+def test_add():
+    assert evaluation.add(2, 3) == 5
 
-    def test_subtract(self):
-        self.assertEqual(evaluation.subtract(5, 3), 2)
+def test_subtract():
+    assert evaluation.subtract(5, 3) == 2
 
 if __name__ == '__main__':
-    unittest.main()
+    # Run tests and generate JUnit XML report
+    if not os.path.exists('test-results'):
+        os.makedirs('test-results')
+    with open('test-results/test_results.xml', 'wb') as output:
+        pytest.main(['--junitxml=test-results/test_results.xml'])
+    output.close()
